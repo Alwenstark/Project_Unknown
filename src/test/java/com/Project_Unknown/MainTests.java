bb9e203f83a247d9dc1b8e5d20b1ad3cb915223c
alwen <alwenperara44@gmail.com>
2024-11-15 19:11:47 +0530

    intial commit

---
+++ src/test/java/com/Project_Unknown/MainTests.java
@@ -0,0 +1,13 @@
+package com.Project_Unknown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
